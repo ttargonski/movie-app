79c29b3b35743245cb5e85007d5e8b2ad34c008f
ttargonski <tomasz.targonski95@gmail.com>
2022-07-01 14:49:22 +0200

    add show/hide genres state to filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,14 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import MovieContext from "../MovieContext";
 import genres from "../genres";
+import { BiArrowFromRight } from "react-icons/bi";
+import { MdMoreHoriz } from "react-icons/md";
 
 const Filter = () => {
   const { setActiveGenre, activeGenre, setFiltered, movies, header } =
     useContext(MovieContext);
+
+  const [moreGenres, setMoreGenres] = useState(false);
 
   useEffect(() => {
     if (activeGenre === 0) {
@@ -19,25 +23,37 @@ const Filter = () => {
 
   return (
     <div className="filter-container">
-      {genres && (
-        <>
+      <button
+        className={activeGenre === 0 ? "active" : null}
+        onClick={() => setActiveGenre(0)}
+      >
+        All
+      </button>
+      <button
+        className={activeGenre === 28 ? "active" : null}
+        onClick={() => setActiveGenre(28)}
+      >
+        Action
+      </button>
+      <button
+        className={activeGenre === 12 ? "active" : null}
+        onClick={() => setActiveGenre(12)}
+      >
+        Adventure
+      </button>
+      {moreGenres &&
+        genres.map((genre) => (
           <button
-            className={activeGenre === 0 ? "active" : null}
-            onClick={() => setActiveGenre(0)}
+            key={genre.id}
+            className={activeGenre === genre.id ? "active" : null}
+            onClick={() => setActiveGenre(genre.id)}
           >
-            All
+            {genre.name}
           </button>
-          {genres.map((genre) => (
-            <button
-              key={genre.id}
-              className={activeGenre === genre.id ? "active" : null}
-              onClick={() => setActiveGenre(genre.id)}
-            >
-              {genre.name}
-            </button>
-          ))}
-        </>
-      )}
+        ))}
+      <button className="more" onClick={() => setMoreGenres(!moreGenres)}>
+        {moreGenres ? <BiArrowFromRight /> : <MdMoreHoriz />}
+      </button>
     </div>
   );
 };
--- src/genres.js
+++ src/genres.js
@@ -1,13 +1,5 @@
 const genres = [
   {
-    id: 28,
-    name: "Action",
-  },
-  {
-    id: 12,
-    name: "Adventure",
-  },
-  {
     id: 16,
     name: "Animation",
   },
